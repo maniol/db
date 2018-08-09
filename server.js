8e86f935326faf038f315311231813fb1343726b
magda <magdalena.aniol@gmail.com>
2018-08-09 20:24:36 +0200

    test with local db

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://magda:112233aa@ds235181.mlab.com:35181/test-db',{useNewUrlParser: true});
-
+//mongoose.connect('mongodb://magda:112233aa@ds235181.mlab.com:35181/test-db',{useNewUrlParser: true});
+mongoose.connect('mongodb://localhost/nodeappdatabase', {
+    useMongoClient: true
+})
 //new user Schema
 const userSchema = new Schema({
     name: String,
